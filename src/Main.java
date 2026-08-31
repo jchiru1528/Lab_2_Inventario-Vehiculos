7e72f6e4ae9bfe67cec6f103d7b6dd15608dfbd2
Jose Chiru <chirujavier12@gmail.com>
2026-08-30 21:43:38 -0500

    Se utiliza la clase Path para la ruta del archivo en el main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-package aplicacion;
-
 import datos.LectorVehiculos;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 import modelo.Vehiculo;
 
 public class Main {
     public static void main(String[] args) {
         LectorVehiculos lector = new LectorVehiculos();
-
+        Path rutaArchivo = Path.of("src", "vehiculos.csv");
         try {
-            List<Vehiculo> vehiculos = lector.cargar("datos/vehiculos.csv");
+            List<Vehiculo> vehiculos = lector.cargar(rutaArchivo.toString());
             System.out.println("=== INVENTARIO DE VEHÍCULOS ===");
             for (Vehiculo vehiculo : vehiculos) {
                 System.out.println(vehiculo.mostrarInformacion());
